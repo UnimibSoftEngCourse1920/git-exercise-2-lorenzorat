3c92da0643ebb33c44f93efbfedf7e8b97452051
Lorenzo Zorat <l.zorat@campus.unimib.it>
2019-10-23 17:19:10 +0200

    aggiunta nuova stampa

--- src/gitExercise2/main/HelloGit.java
+++ src/gitExercise2/main/HelloGit.java
@@ -2,6 +2,7 @@ package gitExercise2.main;
 
 public class HelloGit {
 	public static void main (String [] args) {
-		System.out.println("Questa è una nuova stampa!")
+		System.out.println("Questa è una nuova stampa!");
+		System.out.println("Questa è una stampa!");
 	}
 }
